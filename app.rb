cad2bce0757f91889d34012b93260706712d307e
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-10-25 08:05:50 +0000

    halfway there up to single currency pages

--- app.rb
+++ app.rb
@@ -2,8 +2,46 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  api_url ="https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_RATE_KEY")
+  api_url_list ="https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_RATE_KEY")
   require "http"
+  resp = HTTP.get(api_url_list)
+  raw_resp = resp.to_s
+
   require "json"
+  parsed_resp = JSON.parse(raw_resp)
+  currency_hash = parsed_resp.fetch("currencies")
+
+  @currency_keys = currency_hash.keys
+
   erb(:home)
+end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url_list ="https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_RATE_KEY")
+  
+  require "http"
+  resp = HTTP.get(api_url_list)
+  raw_resp = resp.to_s
+
+  require "json"
+  parsed_resp = JSON.parse(raw_resp)
+  currency_hash = parsed_resp.fetch("currencies")
+
+  @currency_keys = currency_hash.keys
+
+  erb(:single_currency)
+
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url_fx = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+  erb(:convert_currency)
+  
+  # some more code to parse the URL and render a view template
 end
